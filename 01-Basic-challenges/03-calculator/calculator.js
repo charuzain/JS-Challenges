5b1ffd2b1c723f121cddfa254970c5fb2b4ccadd
charuzain <charuabhizain@gmail.com>
2024-03-17 00:09:59 -0400

    Fix bug complete calculator challenge

--- 01-Basic-challenges/03-calculator/calculator.js
+++ 01-Basic-challenges/03-calculator/calculator.js
@@ -1,22 +1,23 @@
 const calculator = (num1, num2, operator) => {
+  let result;
   switch (operator) {
     case '+':
-      return num1 + num2;
+      result= num1 + num2;
       break;
     case '-':
-      return num1 - num2;
+      result= num1 - num2;
       break;
     case '*':
-      return num1 * num2;
+      result= num1 * num2;
       break;
     case '/':
-      return num1 / num2;
+      result= num1 / num2;
       break;
 
     default:
-      return "Enter a Valid number or operator"
-      break;
+      throw new Error("Invalid number or operator")
   }
+  return result;
 }
 
 console.log(calculator(1, 2, '+')) 
